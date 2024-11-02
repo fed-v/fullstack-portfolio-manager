b71c77fb8408f56f206d19e7891c51ef38b079a5
Federico <fed_v@hotmail.com>
2024-11-02 18:55:27 -0400

    Initial commit

---
+++ frontend/server/api/fetchHello.ts
@@ -0,0 +1,11 @@
+export default defineEventHandler(async () => {
+
+  const config = useRuntimeConfig();
+  const response = await fetch(`${config.public.apiBase}/api`);
+  const data = await response.text();
+
+  return {
+    message: data
+  };
+
+});
